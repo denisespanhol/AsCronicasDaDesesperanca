4cab3857b9620500a8afc48df25ab2175910271d
denisespanhol <denisgfespanhol@gmail.com>
2021-12-01 18:15:57 -0300

    Criação do html e css da página de entrada

--- as-cronicas-da-desesperanca/src/pages/StartPage.js
+++ as-cronicas-da-desesperanca/src/pages/StartPage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import "./StartPage.css";
 
 export default function StartPage() {
   return (
     <main>
-      <h1>As Crônicas da Desesperança</h1>
+      <section className="toCentralizeTitle">
+        <p className="asCronicasDa">AS CRÔNICAS DA</p>
+        <p className="desesperança">DESESPERANÇA</p>
+        <p className="eODiaDeCaim">E O DIA DE CAIM</p>
+        <p className="livroI">LIVRO I</p>
+      </section>
+      <footer className="footer toCentralizeFooter">
+        <section className="toCentralizeButtonPrincipal">
+          <button
+            type="button"
+            className="designButtonPrincipal"
+          >
+            IR PARA O SITE
+          </button>
+        </section>
+        <section className="toCentralizeOthersButtons">
+          <button
+            type="button"
+            className="designOthersButtons1"
+          >
+            SOBRE O AUTOR
+          </button>
+          <button
+            type="button"
+            className="designOthersButtons2"
+          >
+            CONHEÇA O PROJETO
+          </button>
+        </section>
+      </footer>
     </main>
   );
 }
