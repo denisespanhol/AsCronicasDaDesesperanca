943acd862a26dd4e36f0b1aadb7ce5752464c158
denisespanhol <denisgfespanhol@gmail.com>
2021-12-13 16:15:48 -0300

    Iniciando a criação do footer

--- as-cronicas-da-desesperanca/src/pages/AboutPage.js
+++ as-cronicas-da-desesperanca/src/pages/AboutPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function AboutPage() {
   return (
     <section>
       <Header />
       <main>ABOUT</main>
+      <Footer />
     </section>
   );
 }
