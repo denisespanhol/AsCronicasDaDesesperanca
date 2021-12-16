c1942f3a0c5083a844b6e25a613865b80cc4cc05
denisespanhol <denisgfespanhol@gmail.com>
2021-12-16 19:49:45 -0300

    Página de seleção de capítulos finalizada para telas menores

--- as-cronicas-da-desesperanca/src/pages/ChaptersPage.js
+++ as-cronicas-da-desesperanca/src/pages/ChaptersPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import testImage from '../images/full-moon-4824456_1280.webp';
+import './ChaptersPage.css';
 
 export default function ChaptersPage() {
   return (
-    <section>
+    <section className='chapterBackground'>
       <Header />
-      <main>CHAPTERS</main>
+      <main>
+        <section className='toCentralizeChapterBox'>
+          <p className='chapterNumber'>PRÓLOGO</p>
+          <p className='chapterTitle'>Uma Luz Verde Num Céu de Maio</p>
+          <section className='chapterBox'>
+            <img src={ testImage } alt='teste' className='coverImage' />
+          </section>
+        </section>
+        <section className='toCentralizeChapterBox'>
+          <p className='chapterNumber'>CAPÍTULO I</p>
+          <p className='chapterTitle'>Sr. Santos</p>
+          <section className='chapterBox'>
+            <img src={ testImage } alt='teste' className='coverImage' />
+          </section>
+          <section className='toCentralizeChapterBox'>
+          <p className='chapterNumber'>CAPÍTULO II</p>
+          <p className='chapterTitle'>Ontem</p>
+          <section className='chapterBox'>
+            <img src={ testImage } alt='teste' className='coverImage' />
+          </section>
+        </section>
+        </section>
+      </main>
       <Footer />
     </section>
   );
