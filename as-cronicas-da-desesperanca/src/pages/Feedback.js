79fd3c18d04c3e38ceecab522d48160cb7aa6068
denisespanhol <denisgfespanhol@gmail.com>
2021-12-30 11:04:41 -0300

    Desenvolvendo o forms

--- as-cronicas-da-desesperanca/src/pages/Feedback.js
+++ as-cronicas-da-desesperanca/src/pages/Feedback.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import divisorImg from '../images/divider-4715969.png';
+import './Feedback.css';
 
 export default function Feedback() {
   return (
-    <section>
+    <section className='feedbackBackground'>
       <Header />
-      <main>
-        Feedback
+      <main className='toCentralizeReadingFeedbackBox'>
+        <section className='readingFeedbackBox'>
+          <section className='titleFeedbackBox'>
+            <p className='feedbackTitle'>ME CONTA O QUE ACHOU!</p>
+          </section>
+          <form className='textFeedbackBox'>
+            <img src={ divisorImg } alt='divisor' className='divisorImgTop' />
+            <label htmlFor='theName'>
+              1 - Nome ou apelido
+              <input
+                id='theName'
+                type='text'
+                placeholder='Insira como deseja se identificar'
+              />
+            </label>
+            <label htmlFor='email'>
+              2 - E-mail
+              <input
+                id='email'
+                type='email'
+                placeholder='ex: pietro_hgatim@gmail.com'
+              />
+            </label>
+            <label htmlFor='chapter'>
+              3 - Escolha o capítulo
+              <select
+                id='chapter'
+                placeholder='Selecione o capítulo'
+              >
+                <option>Prólogo</option>
+                <option>Capítulo I</option>
+              </select>
+            </label>
+          </form>
+        </section>
       </main>
       <Footer />
     </section>
