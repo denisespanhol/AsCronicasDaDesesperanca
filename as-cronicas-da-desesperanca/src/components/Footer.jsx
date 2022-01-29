5c3608250a67a1aca25587540f46e460ef1e7508
denisespanhol <denisgfespanhol@gmail.com>
2022-01-28 22:06:21 -0300

    Refatorando Footer

--- as-cronicas-da-desesperanca/src/components/Footer.jsx
+++ as-cronicas-da-desesperanca/src/components/Footer.jsx
@@ -1,33 +1,10 @@
-import React, { useContext } from 'react';
-import AppContext from '../context/AppContext';
-import solidInstagram from '../images/instagram-square-brands.svg';
+import React from 'react';
+import solidInstagram from '../images/InstaDesesperanca.png';
 import './Footer.css';
 
 export default function Footer() {
-  const { toFixed } = useContext(AppContext);
-
-  let theFooter = {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "auto",
-    backgroundColor: "rgb(18, 17, 17, 1)",
-  };
-
-  if (toFixed && window.innerWidth > 1049) {
-    theFooter = {
-      display: "flex",
-      justifyContent: "center",
-      marginTop: "auto",
-      backgroundColor: "rgb(18, 17, 17, 1)",
-      position: "fixed",
-      bottom: "0",
-      left: "0",
-      width: "100%",
-    }
-  }
-
   return (
-    <footer className='toCentralizeFooter2' style={theFooter}>
+    <footer className='toCentralizeFooter'>
       <p className='eODiaDeCaimFooter'>E o Dia de Caim | Livro I</p>
       <nav className='instaIcon'>
         <img src={ solidInstagram } alt='instaIcon' className='iconSize' />
