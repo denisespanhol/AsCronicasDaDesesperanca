7463062f2eb558911ddb4734132ea5710ddc9c63
denisespanhol <denisgfespanhol@gmail.com>
2022-02-20 12:32:19 -0300

    Criando página do capítulo I

---
+++ as-cronicas-da-desesperanca/src/pages/Chapters/ChapterI.js
@@ -0,0 +1,274 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import prologueCover from '../../images/arte_prólogo.jpg';
+import divisorImg from '../../images/divider-4715969.png';
+import './AllChapters.css';
+
+export default function ChapterI() {
+  return (
+    <section className='allChaptersBackground'>
+      <Header />
+      <main className='toCentralizeReadingAllChaptersBox'>
+        <section className='readingAllChaptersBox'>
+          <section className='titleAllChaptersBox'>
+            <p className='allChaptersNumber'>CAPÍTULO I</p>
+            <p className='allChaptersTitle'>SR. SANTOS</p>
+          </section>
+          <img src={ divisorImg } alt='divisor' className='divisorImgTop' />
+          <img src={ prologueCover } alt='Capa do prólogo' className='coverImg' />
+          <p className='ilustratorCredits'>Ilustração: Paulo Matiazi</p>
+          <img src={ divisorImg } alt='divisor' className='divisorImgTop' />
+          <section className='textAllChaptersBox'>
+            <p>
+              A visão embaçada revelava uma luz do sol tímida, semicortada por uma persiana velha atrás de uma janela manchada. O primeiro impulso do rapaz de olhos amedrontados foi verificar a fisgada doída no pescoço, mas o impulso foi contido. Seu corpo estava imobilizado por amarras de panos velhos em uma cadeira igualmente antiga. Pietro recobrara a consciência.
+            </p>
+            <p>
+              Um minuto acordado foi o suficiente para o rapaz supor que o confundiram com alguém rico, ou que seus órgãos seriam vendidos para o mercado clandestino, ou que fosse vingança pelo mal que fez a alguém, mesmo podendo jurar que nunca fizera mal a ninguém. Tentara se lembrar como havia chegado ali, mas a cada tentativa, sentia pontadas fortes ao redor de sua cabeça, como se agulhas grandes estivessem sendo enfiadas cérebro adentro. O rapaz era medo e confusão. Alguns minutos depois, a porta lascada do pequeno apartamento abriu com um barulho que indicava lascas de madeira quebrando. Um homem alto com um rosto chupado, e olhos assustadoramente arregalados, entrava vestindo um terno branco e brilhante. Os cabelos grisalhos penteados para trás e a vestimenta contrastavam com o pequeno aposento que cheirava a comida estragada. O homem sorriu, quando os olhos trêmulos de Pietro o encararam, e fez uma reverência.
+            </p>
+            <p>
+              — Sr. Santos, estava preocupado com seu constante estado de inconsciência — disse o homem com os olhos arregalados.
+            </p>
+            <p>
+              — E-eu não tenho nada que você possa querer, por favor, eu ainda sou muito jovem para morrer! Sou asmático e tenho muitos problemas de saúde! Vender meus órgãos não vai valer a pena!
+            </p>
+            <p>
+              — Não diga mentiras, Sr. Santos, sua saúde é melhor do que a média.
+            </p>
+            <p>
+              Quanto mais os olhos de Pietro lacrimejavam, mais os do homem de branco arregalavam. Enquanto o jovem amedrontado implorava pela vida, uma risada, que certamente não era do homem assustador, ecoou pelo cômodo. A sensação de arrepio na espinha de Pietro só não foi maior do que o silêncio instaurado no velho quarto.
+            </p>
+            <p>
+              — O-o que foi isso agora? — perguntou Pietro, não sabendo se queria mesmo ouvir a resposta.
+            </p>
+            <p>
+              — Isso o que? — respondeu o homem grisalho inclinando levemente a cabeça para a esquerda — Você ouviu? Os sintomas começaram cedo...
+            </p>
+            <p>
+              — Sintomas? Você me drogou? Por favor, eu juro que se me deixar ir eu não presto queixa... por favor...
+            </p>
+            <p>
+              As lágrimas que rolavam pelo rosto de Pietro pareciam comover o estranho tanto quanto seus pedidos. O homem abriu uma gaveta e retirou um otoscópio prateado. Dirigiu-se primeiro a orelha direita e depois a esquerda. Pietro tremia muito, mas a tremedeira não pareceu ser obstáculo para a curiosidade do estranho.
+            </p>
+            <p>
+              — Interessante... sem danos e nem sangramentos. — Sussurrava o homem com um volume controlado.
+            </p>
+            <p>
+              — O-o que foi injetado em mim? Alguma droga experimental?
+            </p>
+            <p>
+              Aparentemente, a figura de branco parecia ouvir somente o que lhe interessava. Pietro resolveu parar de tentar conversar com seu sequestrador e começou a observar o espaço e pensar em alguma forma de escapar. A visão que tinha da janela era um céu azul imenso, o que o fez concluir que ali não seria uma rota de fuga segura. Não parecia haver outra alternativa senão a porta velha pela qual o homem entrara. Se pudesse liberar os pés, poderia sair dali com cadeira e tudo. Só precisava esperar o homem de olhos grandes ir embora.
+            </p>
+            <p>
+              Um barulho vindo da mesma gaveta que saiu o otoscópio quebrou os pensamentos do rapaz. Agora, o homem de branco retirara uma lanterna de pupila e, antes de usá-la, abaixou sua cabeça até que ficasse com o rosto na mesma altura do rosto de Pietro. A sensação desconfortável de encarar o estranho tão de perto foi substituída por um medo inconsciente, como se aqueles olhos pretos e grandes estivessem invadindo a sua alma lentamente. Quando o homem se levantou, a sensação foi embora junto. Ele segurou a cabeça do rapaz com uma mão, para tremer menos, e com a outra utilizou a lanterna e começou a avaliar as pupilas.
+            </p>
+            <p>
+              — Idade? — Perguntou o aparente doutor, rotineiramente.
+            </p>
+            <p>
+              — D-dezessete.
+            </p>
+            <p>
+              — Raridade. Nessa idade...
+            </p>
+            <p>
+              — Afinal, que porra você tá fazendo?
+            </p>
+            <p>
+              — Mais educação, Sr. Santos.
+            </p>
+            <p>
+              O homem de cabelos grisalhos deu um suspiro, se afastou e começou a falar baixo com o criado mudo, e depois de um tempo, a discutir em sussurros com o móvel. Pietro não vivera o suficiente para morrer nas mãos de um pseudo-médico maluco. Decidiu tentar fazer mais força nas amarras, estava começando a senti-las cederem, mas, enquanto tentava, um apito começou a soar dentro da sua cabeça. No começo, pensou ser um zumbido normal devido a força que estava fazendo, mas depois de alguns segundos, o zumbido aparentava uma locomotiva correndo dentro de sua cabeça, esmagando seus pensamentos com uma força descontrolada. Tudo que o rapaz pode fazer foi gritar.
+            </p>
+            <p>
+              — Está acontecendo. — Disse o homem, como se falasse mais para si mesmo do que para o criado-mudo.
+            </p>
+            <p>
+              A cabeça de Pietro parecia um vulcão prestes a entrar em erupção, sua pele esquentara e as veias do seu cérebro saltavam como se fossem se romper a qualquer segundo. O rapaz começou a se contorcer, as amarras já não pareciam fortes o suficiente para conter os espasmos. Uma dor aguda começou a correr sobre o corpo de Pietro, como se houvessem giletes correndo em suas veias. Foi o último sentimento registrado em sua memória.
+            </p>
+            <p>
+              Pietro sabia que havia desmaiado, se lembrava dos olhos do homem grisalho, da dor e do medo. A sensação do rapaz era a de um mergulho, só que ao invés da água, parecia estar mergulhando em sua alma. Afundou até emergir em um salão de estética lúgubre, quase como se o arquiteto do lugar gostasse de cemitérios, apesar de não haver lápides ali. O jovem medroso não aparentava ter ganho mais coragem. O lugar fazia Pietro sentir saudades do quarto velho onde acordara amarrado. Repetia a todo momento que era um sonho, apesar de estar consciente demais para estar dormindo.
+            </p>
+            <p>
+              O salão era redondo e, para além da lugubridade, possuía janelas com vitrais coloridos em tons de roxo. No canto contrário do espaço parecia haver uma mulher de costas, balbuciando alguma cantiga. Aos poucos, as palavras sussurradas e ritmadas foram sendo compreendidas: <i>“Não feche os olhos ao dormir, não abra a boca ao sorrir, tape os ouvidos antes de ouvir, ou o diabo vai te possuir!”</i>. O rosto de Pietro se contorceu de terror, mais pela visão da figura que cantava do que pela cantiga. A mulher, ao se virar, revelou os olhos costurados, a pele descamada e uma enorme boca com dentes serrados. Como se não bastasse a figura deformada, ela segurava uma cabeça decepada com cabelos negros e meio enrolados, olhos chorosos e uma expressão de desespero acompanhado de um pouco de sardas. A pressão do rapaz baixou quando viu sua própria cabeça nas mãos da criatura.
+            </p>
+            <p>
+              Pietro gritou o mais alto que pôde, mas quando se deu conta, estava novamente no quarto antigo, as velhas amarras haviam sido arrebentadas e, juntamente com a cadeira quebrada, estavam jogadas ao chão. O jovem trêmulo percebeu que parecia haver passado um vendaval no quarto antigo, deixando algumas partes das paredes com buraco, a porta velha partida ao meio e a janela quebrada.
+            </p>
+            <p>
+              — O-o que aconteceu?
+            </p>
+            <p>
+              — Você, Sr. Santos. Você aconteceu. — respondeu o homem grisalho, surgindo debaixo dos destroços da porta, com as roupas um pouco empoeiradas e com os olhos igualmente grandes — Há diversas maneiras de passar pelo segundo nascimento, você certamente pegou um atalho.
+            </p>
+            <p>
+              — Quê?
+            </p>
+            <p>
+              — Seraphim Whiteplum.
+            </p>
+            <p>
+              — É o nome d-da droga?
+            </p>
+            <p>
+              — É o meu nome, Sr. Santos. — Seraphim parecia cada vez mais curioso.
+            </p>
+            <p>
+              — Não precisava t-ter se apresentado, eu realmente não vou prestar queixa, então... — estar sem as amarras fazia Pietro ganhar um pouco mais de coragem —, é só me deixar vazar e todo mundo sai ganhando, certo?
+            </p>
+            <p>
+              — Seria muita irresponsabilidade da minha parte te deixar ir sem alguns apontamentos, e acredite quando eu digo: todo nascimento é doloroso, Sr. Santos.
+            </p>
+            <p>
+              — Porque fica me chamando pelo meu sobrenome?
+            </p>
+            <p>
+              — Polimento e educação.
+            </p>
+            <p>
+              — O-o quê?
+            </p>
+            <p>
+              — Responda-me, Sr. Santos, você acredita em algo que não pode ver? Algo que seus sentidos não captam naturalmente?
+            </p>
+            <p>
+              — T-tipo pessoas mortas?
+            </p>
+            <p>
+              — A vida seria mais simples se o problema fosse os mortos... — respondeu o homem grisalho com um suspiro — não há como explicar com palavras. Eu prefiro que você veja. — Seraphim olhou para o fim do corredor, que agora era visível graças a porta derrubada, como se procurasse alguém. — Hades? Por que ele resolve sumir numa hora dessas?
+            </p>
+            <p>
+              — V-você tem um comparsa? — Perguntou Pietro, tentando repassar mais um plano de fuga na cabeça caso tenha que fugir de uma quadrilha.
+            </p>
+            <p>
+              — É quase isso, é meu... Eu realmente precisaria que você visse antes, ele estava aqui até pouco tempo, ali encima do criado mudo.
+            </p>
+            <p>
+              O jovem esquecera por um segundo que estava lidando com um maluco. Apesar dos pesares, Pietro nunca gostou de machucar ninguém, mas precisava sair dali. Aproveitando-se da falta de atenção de Seraphim, o rapaz deu um impulso em direção ao corredor, e se preparou para dar um empurrão no homem de branco.
+            </p>
+            <p>
+              A primeira surpresa, foi que o pseudo-médico maluco desviou da investida com um bonito passo de dança. Como ele podia agir tão rapidamente se não estava olhando? A segunda surpresa, porém, foi a de que Seraphim nada fez para impedir Pietro de atravessar o corredor. O rapaz torcia para que isso fosse realmente uma boa surpresa.
+            </p>
+            <p>
+              Pietro logo reconheceu a passagem que o levaria até a escadaria, e de lá para fora do prédio velho, por uma porta com um olho-mágico. Mas, quando tudo parecia simples, os olhos do rapaz captaram algo encima de uma mesa, do que parecia ser uma minúscula cozinha. Um escorpião preto com listras azuis, que lembravam pequenas chamas tatuadas num escuro exoesqueleto, e grande o suficiente para ocupar a mesa inteira, olhava para ele, com uma cauda gigante levantada, e aparentemente preparada para atacar.
+            </p>
+            <p>
+              — E-e-e-escorpião! — gritou Pietro enquanto se afastava o máximo que podia.
+            </p>
+            <p>
+              — Perfeito! Como sou feliz por ter te encontrado, Sr. Santos! — disse Seraphim, empolgado, enquanto vinha pelo corredor. — Viu, seu artrópode estressado, tudo correu bem! — Disse Seraphim para o escorpião.
+            </p>
+            <p>
+              — Eu não vejo a hora de você morrer, médico fajuto! Devia se operar para remover esse péssimo senso de merda que você tem! — Respondeu o escorpião com uma voz que parecia a Pietro já ter sido ouvida..., mas não que isto importasse agora, já que o rapaz estava mais surtado do que pensara que Seraphim estivesse.
+            </p>
+            <p>
+              — O-o-o e-escorpião falou! Ele tá falando! Ele vai me matar?
+            </p>
+            <p>
+              — Está feliz agora, Hades? Custava fazer uma introdução decente? — Dizia Seraphim, aparentemente ignorando as ofensas dadas por Hades.
+            </p>
+            <p>
+              — E perder a chance de importunar um fedelho cheirando a mijo? — Ria o escorpião.
+            </p>
+            <p>
+              Seraphim percebeu que o cheiro de mijo não fora figurativo. Pietro se mijara de medo e, mesmo depois da cauda de Hades estar baixa, o rapaz parecia pronto para mijar de novo.
+            </p>
+            <p>
+              Com alguma ajuda do homem grisalho, Pietro conseguiu se sentar em uma cadeira, afastado do escorpião gigante, enquanto tentava fazer toda a força, que deixara seu corpo, voltar.
+            </p>
+            <p>
+              — O-o escorpião e-ele... — Conforme a calma vinha, muitas perguntas surgiam, mas conseguir proferi-las se tornou um desafio.
+            </p>
+            <p>
+              — Pode chamá-lo pelo nome: Hades, e, — suspirou Seraphim — se quiser há papel higiênico no banheiro...
+            </p>
+            <p>
+              — O seu b-bicho é...? — começou Pietro.
+            </p>
+            <p>
+              — Bicho? — Gritou Hades, ofendido. — A cada dia uma humilhação nova!
+            </p>
+            <p>
+              — Menos, Hades. É um recém-nascido que temos aqui!
+            </p>
+            <p>
+              — Seu problema, não meu. Você é o único que pode terminar morto mesmo. — Resmungou o escorpião preto.
+            </p>
+            <p>
+              — Vamos, Sr. Santos, termine a pergunta. — Disse Seraphim.
+            </p>
+            <p>
+              — O-o seu... amigo? Amigo, o seu amigo é... tipo... o que é seu amigo? — Perguntou Pietro, como se a própria pergunta parecesse não expressar exatamente o que queria saber.
+            </p>
+            <p>
+              — Fico feliz que já esteja indo direto ao ponto — Disse Seraphim — Hades é um espírito primordial, Sr. Santos. E, vamos ser sinceros, não é o primeiro espírito que você conhece, não é mesmo?
+            </p>
+            <p>
+              Uma súbita memória surgiu na mente de Pietro. Ele queria um gato, foi até o mercado central, numa loja chamada “Coelho da Amizade”. Então encontrou um. Lembrou-se, inclusive, de que era uma gata, mas como sabia disso? As memórias pareciam cair como âncoras pesadas em sua cabeça. Era pequena, preta, com olhos grandes e amarelos. Lembrou de sentir sua alma vibrar como se houvesse uma ventania percorrendo suas veias.
+            </p>
+            <p>
+              — Seraphim? — perguntou Pietro enquanto fixava o olhar hipnoticamente para os olhos arregalados do médico.
+            </p>
+            <p>
+              — Sim, Sr. Santos? Lembrou de algo?
+            </p>
+            <p>
+              — Exatamente o que é um espírito primordial?
+            </p>
+            <p>
+              — Difícil explicar para um recém-nascido sem referências, mas prometo fazer meu melhor... — respondeu o homem grisalho com os olhos já não tão curiosos. — Existe uma camada de seres vivos que os sentidos humanos não conseguem captar com total frequência. Mas, que, ainda assim, conseguimos nos comunicar durante a vida, através da espiritualidade, fé, crenças, credos e tudo que só pode ser sentido. Os seres vivos dessa camada da existência são chamados de primordiais, espíritos primordiais. Nós só podemos ter esse contato mais visual, tátil e próximo graças ao segundo nascimento. Fui claro?
+            </p>
+            <p>
+              — Está me dizendo que o escorpião...
+            </p>
+            <p>
+              — Hades... — Resmungou Hades.
+            </p>
+            <p>
+              — E-está dizendo que ele é um ser espiritual, tipo um anjo ou demônio, tipo, tipo essas coisas?
+            </p>
+            <p>
+              — É... tudo bem se eu disser que depende? É que são muitos conceitos chatos, estava apostando que iria direto para a parte divertida.
+            </p>
+            <p>
+              — C-como?
+            </p>
+            <p>
+              — Bom, uma coisa de cada vez, Sr. Santos. O mais importante agora é que me conte como foi o seu dia ontem.
+            </p>
+            <p>
+              Um miado atravessou os ouvidos de Pietro fazendo com que seu estômago também miasse de medo. Ao virar a cabeça para a janela do cômodo, o rapaz viu uma gata pequena, preta e de olhos amarelos, sentada. Não sabia bem como, mas tinha certeza que a felina ria.
+            </p>
+            <p>
+              — Ah, eu e Hades estávamos sentindo sua falta! — Disse Seraphim, hospitaleiro, para a gata.
+            </p>
+            <p>
+              — E-eu lembro! — Disse Pietro. Enquanto seus olhos trocavam uma fria conexão com os olhos cor de topázio da felina, o rapaz sentiu uma última âncora cair no seu lapso de memória. — Bastet!
+            </p>
+            <img src={ divisorImg } alt='divisor' className='divisorImgBot' />
+            <p className='theEnd'>FIM DO CAPÍTULO I</p>
+          </section>
+          <section className='endButtons'>
+            <Link to='/feedback' style={{ textDecoration: 'none' }}>
+              <button
+                type='button'
+                className='designEndButtons'
+              >
+                Dê seu Feedback!
+              </button>
+            </Link>
+              <button
+                type='button'
+                className='designEndButtonsDisabled'
+                disabled={ true }
+              >
+                Próximo capítulo
+              </button>
+          </section>
+        </section>
+      </main>
+      <Footer />
+    </section>
+  )
+}
