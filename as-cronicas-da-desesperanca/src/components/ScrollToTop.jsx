7a6295a73cbd128f36995dd56fd6e0cb76918d96
denisespanhol <denisgfespanhol@gmail.com>
2022-02-20 14:13:51 -0300

    Inserindo código para levar a rota para o topo da página quando um link de rota é ativado

---
+++ as-cronicas-da-desesperanca/src/components/ScrollToTop.jsx
@@ -0,0 +1,19 @@
+// Código para iniciar todas as páginas de rotas pelo topo aprendido no seguinte link: https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
+
+import { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+function ScrollToTop({ history }) {
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return () => {
+      unlisten();
+    }
+  });
+
+  return (null);
+}
+
+export default withRouter(ScrollToTop);
